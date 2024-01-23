82cf0ef39955b3a33f7a46ced872c36132a73e2d
prograin <143938977+voidProgramms@users.noreply.github.com>
2024-01-23 19:48:58 +0300

    add russian chars

--- calculator/calculator/calculator.cpp
+++ calculator/calculator/calculator.cpp
@@ -37,17 +37,17 @@ int main() {
     setlocale(LC_ALL, "Rus");
     bool work = true;
     do {
-        cout << "Âûáåðèòå îïåðàöèþ:" << endl;
-        cout << "1. Ñëîæèòü 2 ÷èñëà" << endl;
-        cout << "2. Âû÷åñòü âòîðîå èç ïåðâîãî" << endl;
-        cout << "3. Ïåðåìíîæèòü äâà ÷èñëà" << endl;
-        cout << "4. Ðàçäåëèòü ïåðâîå íà âòîðîå" << endl;
-        cout << "5. Âîçâåñòè â ñòåïåíü N ïåðâîå ÷èñëî" << endl;
-        cout << "6. Íàéòè êâàäðàòíûé êîðåíü èç ÷èñëà" << endl;
-        cout << "7. Íàéòè 1 ïðîöåíò îò ÷èñëà" << endl;
-        cout << "8. Íàéòè ôàêòîðèàë èç ÷èñëà" << endl;
-        cout << "9. Âûéòè èç ïðîãðàììû" << endl << endl;
-        cout << "Âàøà êîìàíäà: ";
+        cout << "Выберите операцию:" << endl;
+        cout << "1. Сложить 2 числа" << endl;
+        cout << "2. Вычесть второе из первого" << endl;
+        cout << "3. Перемножить два числа" << endl;
+        cout << "4. Разделить первое на второе" << endl;
+        cout << "5. Возвести в степень N первое число" << endl;
+        cout << "6. Найти квадратный корень из числа" << endl;
+        cout << "7. Найти 1 процент от числа" << endl;
+        cout << "8. Найти факториал из числа" << endl;
+        cout << "9. Выйти из программы" << endl << endl;
+        cout << "Ваша команда: ";
         string operation;
         cin >> operation;
         cout << endl;
@@ -77,7 +77,7 @@ int main() {
         case 4: {
             numbers = Calculator::getNums(2);
             if (numbers[1] == 0) {
-                cout << "Íà íîëü äåëèòü íåëüçÿ(" << endl;
+                cout << "На ноль делить нельзя(" << endl;
                 cout << endl;
             }
             else {
@@ -98,7 +98,7 @@ int main() {
         case 6: {
             numbers = Calculator::getNums(1);
             double res = sqrt(numbers[0]);
-            cout << "Êîðåíü èç " << numbers[0] << " = " << res << endl;
+            cout << "Корень из " << numbers[0] << " = " << res << endl;
             cout << endl;
             delete[] numbers;
             break;
@@ -106,7 +106,7 @@ int main() {
         case 7: {
             numbers = Calculator::getNums(1);
             double res = numbers[0] * 0.01;
-            cout << "1% îò " << numbers[0] << " = " << res << endl;
+            cout << "1% от " << numbers[0] << " = " << res << endl;
             cout << endl;
             delete[] numbers;
             break;
@@ -119,7 +119,7 @@ int main() {
                 cout << endl;
             }
             else {
-                cout << "Íåëüçÿ âû÷èñëèòü ôàêòîðèàë èç îòðèöàòåëüíîãî ÷èñëà(" << endl;
+                cout << "Нельзя вычислить факториал из отрицательного числа("<< endl;
                 cout << endl;
             }
             delete[] numbers;
@@ -127,11 +127,11 @@ int main() {
         }
         case 9: {
             work = false;
-            cout << "Âû÷èñëåíèÿ çàâåðøåíû!" << endl;
+            cout << "Вычисления завершены!" << endl;
             break;
         }
         default: {
-            cout << "ß íå çíàþ òàêóþ êîìàíäó(" << endl;
+            cout << "Я не знаю такую команду(" << endl;
             cout << endl;
             break;
         }
