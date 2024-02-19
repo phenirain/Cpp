0b6ddcaeda315d09eedcd1f54124a1ba0958d7bc
voidProgramms <isip_r.o.biktimirov@mpt.ru>
2024-02-19 16:08:21 +0300

    matrix is already

---
+++ matrix/matrix/matrix.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double arr[3][5];
+    double sum = 0.0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            double n;
+            cout << "Input a value:";
+            cin >> n;
+            arr[i][j] = n;
+            sum += n;
+        }
+    }
+    cout << "Average of matrix: " << sum;
+    return 0;
+}
